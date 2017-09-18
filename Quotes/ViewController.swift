6996fef1364cbfcf9509a51cfb0150b2949c0c28
Muhammad Noor <N00@liquid.local>
2017-09-18 23:37:08 +0700

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Muhammad Noor on 18/09/2017.
+//  Copyright © 2017 Muhammad Noor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
